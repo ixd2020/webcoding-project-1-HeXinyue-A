ec73302f2eceecdded7cb7aaa827a0e94a8605b1
Glitch (webcoding) <none>
2020-12-18 23:30:43 +0000

    🎻🍕 Updated with Glitch

--- jquery.js
+++ jquery.js
@@ -1,10 +1,16 @@
-// $('#myButton').click(function() {
-//    $.scrollTo($('#wrap'), 1000);
-// });
-"use strict";
 
-$("#button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#anotherdiv").offset().top
-    }, 2000);
-});
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
